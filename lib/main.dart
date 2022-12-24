e9f931a644fa14ba86030f62e49ced0c8156117e
Tushar Chavan <tusharrchavan014@gmail.com>
2022-12-25 03:54:29 +0530

    Changed icon

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WhatsApp',
+      title: 'WhatsApp X',
       theme: lightTheme(),
       darkTheme: darkTheme(),
       themeMode: ThemeMode.system,
